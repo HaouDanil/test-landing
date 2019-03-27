16009cb5bbbc84abb5a6f8cac29b2570a41b1ce4
Danil <danil.haou@gmail.com>
2019-03-27 23:21:01 +0300

    Improved loading speed, fixied bugs. It's end is summary.

--- js/main.js
+++ js/main.js
@@ -30,4 +30,14 @@ $(window).load(function() {
     return false;
   });
   $('input[type="tel"]').mask("+7 (999) 999-99-99");
+  //Show map only when user scrolled up to her
+  let reviews = $('.reviews');
+  let reviewsTop = reviews.offset().top;
+  $(window).bind('scroll', function(){
+    let windowTop = $(this).scrollTop();
+    if(windowTop > reviewsTop) {
+      $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A6be7d7a1cb2b04a050e87108f0f9ff08906ee7ef7ff5b9f8aa451112bcdd0b3a&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>');
+      $(window).unbind();
+    }
+  });
 });
